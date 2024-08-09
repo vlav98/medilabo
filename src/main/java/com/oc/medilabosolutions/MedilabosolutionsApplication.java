171b77de81c0c26a9e9668e61cc870ec64d6f5b6
Valentine Lav <valentinelav98@gmail.com>
2024-08-09 21:59:27 +0200

    :tada: MediLabo project initialisation

---
+++ src/main/java/com/oc/medilabosolutions/MedilabosolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.oc.medilabosolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilabosolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilabosolutionsApplication.class, args);
+	}
+
+}
