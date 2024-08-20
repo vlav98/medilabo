ee7df3ff2697b558a99d3b272e312b2b1991393c
Valentine Lav <valentinelav98@gmail.com>
2024-08-21 00:17:05 +0200

    :card_file_box: Add SQL files

---
+++ src/main/resources/database/create_table.sql
@@ -0,0 +1,12 @@
+DROP TABLE patients CASCADE;
+
+CREATE TABLE patients
+(
+    patient_id     BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name     VARCHAR(255) NULL,
+    last_name      VARCHAR(255) NULL,
+    birth_date     date         NOT NULL,
+    gender         VARCHAR(255) NOT NULL,
+    postal_address VARCHAR(255) NULL,
+    phone_number   VARCHAR(255) NULL
+);
