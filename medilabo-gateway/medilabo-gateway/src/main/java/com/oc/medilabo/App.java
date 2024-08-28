2e12c49460cc6d50bc4e16c4cb0fe533598d0dc0
Valentine Lav <valentinelav98@gmail.com>
2024-08-28 15:55:33 +0200

    :sparkles: Add Gateway microservice

---
+++ medilabo-gateway/medilabo-gateway/src/main/java/com/oc/medilabo/App.java
@@ -0,0 +1,13 @@
+package com.oc.medilabo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
