5febe391befcfdfbec8aaee4a139c7f91c91828c
Valentine Lav <valentinelav98@gmail.com>
2024-09-04 17:17:50 +0200

    :wrench: Fix pom.xml

--- medilabo-patients/src/main/java/com/oc/medilabosolutions/MedilaboPatientsApplication.java
+++ medilabo-patients/src/main/java/com/oc/medilabosolutions/MedilaboPatientsApplication.java
@@ -2,12 +2,10 @@ package com.oc.medilabosolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableDiscoveryClient
 public class MedilaboPatientsApplication {
 
 	public static void main(String[] args) {
--- medilabo-risk-report/src/main/java/com/oc/medilabo/MedilaboRiskReportApplication.java
+++ medilabo-risk-report/src/main/java/com/oc/medilabo/MedilaboRiskReportApplication.java
@@ -2,12 +2,10 @@ package com.oc.medilabo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MedilaboRiskReportApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedilaboRiskReportApplication.class, args);
