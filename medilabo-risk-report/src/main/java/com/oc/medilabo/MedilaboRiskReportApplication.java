061fe0926d49ce9d98e2be10c6058c07735146a0
Valentine Lav <valentinelav98@gmail.com>
2024-09-05 10:11:31 +0200

    :wrench: Add config file

--- medilabo-risk-report/src/main/java/com/oc/medilabo/MedilaboRiskReportApplication.java
+++ medilabo-risk-report/src/main/java/com/oc/medilabo/MedilaboRiskReportApplication.java
@@ -2,17 +2,10 @@ package com.oc.medilabo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MedilaboRiskReportApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedilaboRiskReportApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
