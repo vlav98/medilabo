46ee2df1ad64af2bf8a0d97d76247bc8629db4b1
Valentine Lav <valentinelav98@gmail.com>
2024-09-11 23:17:25 +0200

    :adhesive_bandage: Fix patients & notes services config

--- medilabo-patients/src/main/java/com/oc/medilabosolutions/MedilaboPatientsApplication.java
+++ medilabo-patients/src/main/java/com/oc/medilabosolutions/MedilaboPatientsApplication.java
@@ -2,10 +2,12 @@ package com.oc.medilabosolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class MedilaboPatientsApplication {
 
 	public static void main(String[] args) {
